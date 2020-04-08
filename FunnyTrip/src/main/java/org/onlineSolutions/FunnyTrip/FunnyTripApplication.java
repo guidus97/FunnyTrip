e1a5ca64e869e69d8692719ad8d3fb6a25abf9d2
Guido <guidoruggieri97@gmail.com>
2020-04-08 12:28:25 +0200

    SpringBoot project server created

---
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
@@ -0,0 +1,13 @@
+package org.onlineSolutions.FunnyTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyTripApplication.class, args);
+	}
+
+}
---
+++ FunnyTrip/src/test/java/org/onlineSolutions/FunnyTrip/FunnyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onlineSolutions.FunnyTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
