ec0e04d8d73541d7f467709a8d8f8d623bb54112
Guido <guidoruggieri97@gmail.com>
2020-04-09 21:10:31 +0200

    User model class added

---
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/model/user/User.java
@@ -0,0 +1,105 @@
+package org.onlineSolutions.FunnyTrip.model.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String userName;
+	private String password;
+	private String name;
+	private String lastName;
+	private String email;
+	private boolean active;
+	private String roles;
+
+	public User() {
+	
+	}
+
+	public User(int id, String userName, String password, String name, String lastName, String email, boolean active,
+			String roles) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.name = name;
+		this.lastName = lastName;
+		this.email = email;
+		this.active = active;
+		this.roles = roles;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+}
