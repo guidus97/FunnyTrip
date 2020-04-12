9939171954bf65be1b6943c6236fa8ce3cb7a13c
Guido <guidoruggieri97@gmail.com>
2020-04-12 21:52:00 +0200

    Defined spring security configurations

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/config/security/SecurityConfig.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/config/security/SecurityConfig.java
@@ -1,22 +1,71 @@
 package org.onlineSolutions.FunnyTrip.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
+	
+	@Autowired
+	private UserDetailsService cuds;
+	
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		auth.userDetailsService(cuds).passwordEncoder(getPasswordEncoder());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		http.cors().and().csrf().disable();
+		
+		http.authorizeRequests()
+			.antMatchers("/**/signIn/*", "/api/auth/login", "/api/auth/register", "/login").permitAll()
+			.antMatchers("/api/user/services/**").hasRole("USER")
+			.antMatchers("/api/admin/services/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and().apply(new JwtConfigurer(jwtTokenProvider));
+		
+		http.exceptionHandling().accessDeniedPage("/login");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+		web.ignoring().antMatchers("/*/")
+        .antMatchers("/eureka/**")
+        .antMatchers(HttpMethod.OPTIONS, "/**");
 	}
 	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return cuds;
+	}
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	
 }
