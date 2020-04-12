648109fc89d124d0f4f540b843ae11c1ebd841eb
Guido <guidoruggieri97@gmail.com>
2020-04-12 21:53:15 +0200

    Added annotations and properties for zuul and eureka

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
@@ -1,9 +1,16 @@
 package org.onlineSolutions.FunnyTrip;
 
+import org.onlineSolutions.FunnyTrip.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaServer
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class FunnyTripApplication {
 
 	public static void main(String[] args) {
