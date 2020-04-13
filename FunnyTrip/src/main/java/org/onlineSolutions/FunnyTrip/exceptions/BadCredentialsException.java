f51253a1ab1e8432897610d734b8f43aa2d1db2a
Guido <guidoruggieri97@gmail.com>
2020-04-13 22:13:16 +0200

    Updated class

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/exceptions/BadCredentialsException.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/exceptions/BadCredentialsException.java
@@ -1,5 +1,9 @@
 package org.onlineSolutions.FunnyTrip.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Username or password not valid")
 public class BadCredentialsException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
