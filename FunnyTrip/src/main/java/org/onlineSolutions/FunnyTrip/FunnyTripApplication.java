08a5a3ad290ca4a8282fe184548f5472f04b7a31
Guido <guidoruggieri97@gmail.com>
2020-04-15 21:35:17 +0200

    Changed packaging to war

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
@@ -3,6 +3,8 @@ package org.onlineSolutions.FunnyTrip;
 import org.onlineSolutions.FunnyTrip.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,10 +13,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableZuulProxy
 @EnableEurekaServer
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
-public class FunnyTripApplication {
+public class FunnyTripApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunnyTripApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FunnyTripApplication.class);
 	}
 
 }
