ef98ff69ac46efddd4265d91e92b8028650c11c0
Guido <guidoruggieri97@gmail.com>
2020-04-20 11:50:48 +0200

    Fixed this class adding the bcrypt encoder

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/service/registerService/RegistrationService.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/service/registerService/RegistrationService.java
@@ -3,6 +3,7 @@ package org.onlineSolutions.FunnyTrip.service.registerService;
 import org.onlineSolutions.FunnyTrip.model.user.User;
 import org.onlineSolutions.FunnyTrip.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,13 @@ public class RegistrationService implements I_RegistrationService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+	
 	@Override
 	public User registerUser(User user) {
+		
+		user.setPassword(bcrypt.encode(user.getPassword()));
 		
 		return userRepository.save(user);
 	}
