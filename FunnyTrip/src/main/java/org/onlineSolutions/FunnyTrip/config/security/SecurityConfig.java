b27287460fc2fc3d097f604128846fe2dd9c20c5
Guido <guidoruggieri97@gmail.com>
2020-04-21 18:38:56 +0200

    Modified security settings

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/config/security/SecurityConfig.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.onlineSolutions.FunnyTrip.config.security;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,63 +15,64 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private UserDetailsService cuds;
-	
+
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(cuds).passwordEncoder(getPasswordEncoder());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.cors().and().csrf().disable();
-		
-		http.authorizeRequests()
-			.antMatchers("/**/signIn/*", "/api/auth/login", "/api/auth/registerUser", "/login", "/swagger-ui").permitAll()
-			.antMatchers("/api/user/services/**").hasAuthority("ROLE_USER")
-			.antMatchers("/api/admin/services/**").hasAuthority("ROLE_ADMIN")
-			.antMatchers("/api/account/**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
-			.antMatchers("/api/admin/privileges/**").hasRole("ADMIN")
-			.anyRequest().authenticated()
-			.and().apply(new JwtConfigurer(jwtTokenProvider));
-		}
+
+		http.cors().and().csrf().disable().authorizeRequests()
+				.antMatchers("/**/signIn/*", "/api/auth/login", "/api/auth/registerUser", "/login", "/swagger-ui").permitAll()
+				.antMatchers("/api/user/services/**").hasAuthority("USER")
+				.antMatchers("/api/admin/services/**").hasAuthority("ADMIN")
+				.antMatchers("/api/account/**").hasAnyAuthority("USER", "ADMIN").antMatchers("/api/admin/privileges/**").hasAuthority("ADMIN")
+				.anyRequest().authenticated().and().apply(new JwtConfigurer(jwtTokenProvider))
+				.and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+
+	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 
-		web.ignoring().antMatchers("/*/")
-        .antMatchers("/eureka/**")
-        .antMatchers(HttpMethod.OPTIONS, "/**")
-        .antMatchers("/swagger-ui.html")
-        .antMatchers("/swagger-resources/**")
-        .antMatchers("/webjars/**")
-        .antMatchers("/v2/api-docs");
+		web.ignoring().antMatchers("/*/").antMatchers("/eureka/**").antMatchers(HttpMethod.OPTIONS, "/**")
+				.antMatchers("/swagger-ui.html").antMatchers("/swagger-resources/**").antMatchers("/webjars/**")
+				.antMatchers("/v2/api-docs");
 	}
-	
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	@Bean
+	public AuthenticationEntryPoint unauthorizedEntryPoint() {
+		return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+				"Unauthorized");
+	}
+
 	@Override
 	protected UserDetailsService userDetailsService() {
 		return cuds;
 	}
-	
+
 	@Override
 	@Bean
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
+
 }
