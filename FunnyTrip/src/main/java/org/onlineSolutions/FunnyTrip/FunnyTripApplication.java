63873a48e9664b695189a258dcd19e08ea80e534
Guido <guidoruggieri97@gmail.com>
2020-04-22 21:58:06 +0200

    Redis cache enabled

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/FunnyTripApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaServer
+@EnableCaching
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class FunnyTripApplication extends SpringBootServletInitializer {
 
