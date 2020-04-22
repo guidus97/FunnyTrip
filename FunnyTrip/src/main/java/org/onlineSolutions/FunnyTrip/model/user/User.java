d2e0d03017e299b835256ce206adb47aa3e5fae3
Guido <guidoruggieri97@gmail.com>
2020-04-22 21:28:01 +0200

    Implements Serializable interface

--- FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/model/user/User.java
+++ FunnyTrip/src/main/java/org/onlineSolutions/FunnyTrip/model/user/User.java
@@ -1,5 +1,7 @@
 package org.onlineSolutions.FunnyTrip.model.user;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +14,10 @@ import io.swagger.annotations.ApiModelProperty;
 @Entity
 @Table(name = "\"User\"")
 @ApiModel(description = "Describe User model")
-public class User {
+public class User implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
