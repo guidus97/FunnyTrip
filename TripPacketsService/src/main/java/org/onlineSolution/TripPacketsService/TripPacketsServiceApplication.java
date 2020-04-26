3e40a656b7f614a5956f2c71e2473e4562f66929
Guido <guidoruggieri97@gmail.com>
2020-04-26 12:19:21 +0200

    Enabled properties encryption

--- TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/TripPacketsServiceApplication.java
+++ TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/TripPacketsServiceApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEncryptableProperties
 public class TripPacketsServiceApplication {
 
 	public static void main(String[] args) {
