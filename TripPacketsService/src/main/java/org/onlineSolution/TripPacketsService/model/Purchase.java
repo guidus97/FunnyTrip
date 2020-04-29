0fdc230384795f325910cb94d964e2b7cb6db613
Guido <guidoruggieri97@gmail.com>
2020-04-29 11:10:39 +0200

    Implements Serializable interface

--- TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/model/Purchase.java
+++ TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/model/Purchase.java
@@ -1,5 +1,59 @@
 package org.onlineSolution.TripPacketsService.model;
 
-public class Purchase {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Purchase implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private int trip_pack_id;
+	private int user_id;
+	
+	public Purchase(int id, int trip_pack_id, int user_id) {
+		super();
+		this.id = id;
+		this.trip_pack_id = trip_pack_id;
+		this.user_id = user_id;
+	}
+	
+	public Purchase() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTrip_pack_id() {
+		return trip_pack_id;
+	}
+
+	public void setTrip_pack_id(int trip_pack_id) {
+		this.trip_pack_id = trip_pack_id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	
+	
 }
