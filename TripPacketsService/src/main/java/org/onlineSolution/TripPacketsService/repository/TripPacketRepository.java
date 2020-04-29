0255cf9e701ab6584a6d95a73128304d5c72745c
Guido <guidoruggieri97@gmail.com>
2020-04-29 11:14:43 +0200

    Added custom queries to the two methods

--- TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/repository/TripPacketRepository.java
+++ TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/repository/TripPacketRepository.java
@@ -1,10 +1,24 @@
 package org.onlineSolution.TripPacketsService.repository;
 
+import java.util.List;
+
 import org.onlineSolution.TripPacketsService.model.TripPacket;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TripPacketRepository extends CrudRepository<TripPacket, Integer>{
 
+	@Query(value = "select tp.id, tp.\"name\", tp.\"start\", tp.end_trip, tp.price from public.trip_packet tp\r\n" + 
+			"INNER join purchase on tp.id = purchase.trip_pack_id\r\n" + 
+			"INNER join \"user\" on purchase.user_id = \"user\".id", nativeQuery = true)
+	List<TripPacket> getTripPacketsForAllUsers();
+	
+	@Query(value = "select tp.id, tp.\"name\", tp.\"start\", tp.end_trip, tp.price from public.trip_packet tp\r\n" + 
+			"INNER join purchase on tp.id = purchase.trip_pack_id\r\n" + 
+			"INNER join \"user\" on purchase.user_id = \"user\".id\r\n" + 
+			"where \"user\".username = :username", nativeQuery = true)
+	List<TripPacket> getTripPacketsForSpecificUser(@Param("username") int user_id);
 }
