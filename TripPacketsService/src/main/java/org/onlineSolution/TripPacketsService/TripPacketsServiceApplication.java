cda028de9c5efa89eb576a7e5f7a5e074da31394
Guido <guidoruggieri97@gmail.com>
2020-05-04 22:21:44 +0200

    Caching server side with Redis activated

--- TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/TripPacketsServiceApplication.java
+++ TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/TripPacketsServiceApplication.java
@@ -2,6 +2,7 @@ package org.onlineSolution.TripPacketsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
@@ -9,6 +10,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEncryptableProperties
+@EnableCaching
 public class TripPacketsServiceApplication {
 
 	public static void main(String[] args) {
