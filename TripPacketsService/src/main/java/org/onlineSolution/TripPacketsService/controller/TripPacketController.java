20f95ece784c4e323d9f86e76f8bb8f7b43f4197
Guido <guidoruggieri97@gmail.com>
2020-05-04 22:19:26 +0200

    Added endpoints

--- TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/controller/TripPacketController.java
+++ TripPacketsService/src/main/java/org/onlineSolution/TripPacketsService/controller/TripPacketController.java
@@ -1,5 +1,8 @@
 package org.onlineSolution.TripPacketsService.controller;
 
+import java.util.List;
+
+import org.onlineSolution.TripPacketsService.model.Optional;
 import org.onlineSolution.TripPacketsService.model.TripPacket;
 import org.onlineSolution.TripPacketsService.service.I_TripPacketService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +31,41 @@ public class TripPacketController {
 	public ResponseEntity<TripPacket> getPacketById(@PathVariable("id") int id){
 		
 		return new ResponseEntity<TripPacket>(packetService.getById(id), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/getAllPacketsPurchased", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public ResponseEntity<List<TripPacket>> getAllPurchasedPacketsFromAllUsers(){
+		
+		return new ResponseEntity<List<TripPacket>>(packetService.getAllTripPacketsForAllUsers(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/getAllPacketsForUser/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public ResponseEntity<List<TripPacket>> getAllPurchasedPacketsForUserById(@PathVariable("id") int id){
+		
+		return new ResponseEntity<List<TripPacket>>(packetService.getAllTripPacketsByUserId(id), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public ResponseEntity<List<TripPacket>> getAll(){
+		
+		return new ResponseEntity<List<TripPacket>>(packetService.getAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/updatePacket/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
+	public ResponseEntity<TripPacket> updatePacket(@RequestBody TripPacket tripPacket, @PathVariable("id") int id){
+		
+		return new ResponseEntity<TripPacket>(packetService.updateById(tripPacket, id), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/deletePacket/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletePacket(@PathVariable("id") int id){
+		
+		packetService.deletePacket(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/getPacket/{id}/addOptional", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
+	public ResponseEntity<Optional> addOptionalToPacket(@RequestBody Optional optional, @PathVariable("id") int id) throws Exception{
+		return new ResponseEntity<Optional>(packetService.addOptionalToTripPacket(optional, id), HttpStatus.CREATED);
 	}
 }
