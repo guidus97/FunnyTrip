dc49ff2965f7ebbfbe79c9a51b9bb9214422850a
Guido <guidoruggieri97@gmail.com>
2020-05-31 18:45:03 +0200

    Changes for this file

--- RoomService/main/index.js
+++ RoomService/main/index.js
@@ -3,12 +3,15 @@ var api = require('../factory/routerFactory.js')
 var express = require('express')
 var app = express()
 var body_parser = require('body-parser')
-var port = process.argv[2]
+var port = Number.parseInt(process.argv[2]) || 3000
 var RoomController = require('../router/controller.js')
+var eureka = require('../config/eureka.js')
 
 app.use(body_parser.json())
 app.use('/roomService', router)
 router.use('/methods', api)
+
+eureka(port)
 
 var room = new RoomController(api)
 
@@ -16,3 +19,5 @@ app.listen(port , () => {
     console.log('Listening on port: ' + port);
     
 })
+
+module.exports = app
